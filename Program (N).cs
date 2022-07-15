64713a4a44c1e47e4fd8ca6377d2b2255085a240
Ekaterina Shevchenko <ekaterina.shevchenko@finteco.io>
2022-07-15 11:28:51 +0300

    Innitial commit

---
+++ Program (N).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeSamplesFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int summ = 0;
+            int j = 100 * b + c;
+            for (int i = 0; i < a - 1; i++)
+            {
+                summ = summ + j;
+            }
+
+            Console.WriteLine(summ);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program HW3 on lesson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeSamplesFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int summ = 0;
+            int j = 100 * b + c;
+            for (int i = 0; i < a-1; i++)
+            {
+                summ = summ + j;
+            }
+
+            summ = summ - c;
+            Console.WriteLine(summ);
+            Console.ReadKey();
+
+        }
+    }
+}
